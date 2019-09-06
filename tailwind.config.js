cc8a3858e77d169d26b9b49ccfafcfa9c79fee1a
Jon Thomas <jon.thomas@bunn.com>
2019-09-06 14:30:47 -0500

    Add additional screen breakpoint

--- tailwind.config.js
+++ tailwind.config.js
@@ -147,6 +147,7 @@ module.exports = {
         },
 
         screens: {
+            '0-sm':     '320px',
             'sm':       '480px',
             'sm-md':    '620px',
             'md':       '768px',
