14140495529cce04287e7d099bb7cc18f141e7a3
Jon Thomas <jon.thomas@bunn.com>
2019-10-18 16:03:29 -0500

    Improve bundled vendor files and design system output

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,60 +1,58 @@
-const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight")
+const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight')
 
 module.exports = eleventyConfig => {
+  // Add syntax highlighting
+  eleventyConfig.addPlugin(syntaxHighlight)
 
-    // Add syntax highlighting
-    eleventyConfig.addPlugin(syntaxHighlight);
+  // Add a readable date formatter filter to Nunjucks
+  eleventyConfig.addFilter('dateDisplay', require('./filters/dates.js'))
 
-    // Add a readable date formatter filter to Nunjucks
-    eleventyConfig.addFilter("dateDisplay", require("./filters/dates.js"))
+  // Add a HTML timestamp formatter filter to Nunjucks
+  eleventyConfig.addFilter('htmlDateDisplay', require('./filters/timestamp.js'))
 
-    // Add a HTML timestamp formatter filter to Nunjucks
-    eleventyConfig.addFilter("htmlDateDisplay", require("./filters/timestamp.js"))
+  // Collections
+  eleventyConfig.addCollection('brandSorted', collection => {
+    return collection.getFilteredByTag('brand').sort((a, b) => {
+      if (a.data.title > b.data.title) return -1
+      else if (a.data.title < b.data.title) return 1
+      else return 0
+    }).reverse()
+  })
 
-    // Collections
-    eleventyConfig.addCollection('brandSorted', collection => {
-        return collection.getFilteredByTag('brand').sort((a, b) => {
-            if (a.data.title > b.data.title) return -1;
-            else if (a.data.title < b.data.title) return 1;
-            else return 0;
-        }).reverse()
-    })
+  eleventyConfig.addCollection('componentsSorted', collection => {
+    return collection.getFilteredByTag('components').sort((a, b) => {
+      if (a.data.title > b.data.title) return -1
+      else if (a.data.title < b.data.title) return 1
+      else return 0
+    }).reverse()
+  })
 
-    eleventyConfig.addCollection('componentsSorted', collection => {
-        return collection.getFilteredByTag('components').sort((a, b) => {
-            if (a.data.title > b.data.title) return -1;
-            else if (a.data.title < b.data.title) return 1;
-            else return 0;
-        }).reverse()
-    })
+  eleventyConfig.addCollection('modules', collection => {
+    return collection.getFilteredByTag('modules')
+  })
 
-    eleventyConfig.addCollection('modules', collection => {
-        return collection.getFilteredByTag('modules');
-    })
+  eleventyConfig.addCollection('utilities', collection => {
+    return collection.getFilteredByTag('utilities')
+  })
 
-    eleventyConfig.addCollection('utilities', collection => {
-        return collection.getFilteredByTag('utilities');
-    })
+  // Include our static assets
+  eleventyConfig.addPassthroughCopy('design-system/')
+  eleventyConfig.addPassthroughCopy('docs/')
+  eleventyConfig.addPassthroughCopy('images')
+  eleventyConfig.addPassthroughCopy('fonts')
 
-    // Include our static assets
-    eleventyConfig.addPassthroughCopy("design-system/")
-    eleventyConfig.addPassthroughCopy("docs/")
-    eleventyConfig.addPassthroughCopy("images")
-    eleventyConfig.addPassthroughCopy("fonts")
+  return {
+    templateFormats: ['md', 'njk'],
+    markdownTemplateEngine: 'njk',
+    htmlTemplateEngine: 'njk',
+    passthroughFileCopy: true,
 
-    return {
-        templateFormats: ["md", "njk"],
-        markdownTemplateEngine: 'njk',
-        htmlTemplateEngine: 'njk',
-        passthroughFileCopy: true,
-
-        dir: {
-            input: 'site',
-            output: 'dist',
-            includes: '_includes',
-            layouts: '_layouts',
-            data: '_globals'
-        }
+    dir: {
+      input: 'site',
+      output: 'dist',
+      includes: '_includes',
+      layouts: '_layouts',
+      data: '_globals'
     }
-
+  }
 }
